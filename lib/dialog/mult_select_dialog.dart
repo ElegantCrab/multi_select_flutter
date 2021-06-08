29287040852cd45c3c29419133fe9e9e5b9157ba
Ivan Valenzuela Galaviz <elegantcrab.git@gmail.com>
2021-06-08 12:21:50 -0700

    Flexible widget removed from umlti select dialog

--- lib/dialog/mult_select_dialog.dart
+++ lib/dialog/mult_select_dialog.dart
@@ -110,7 +110,7 @@ class _MultiSelectDialogState<V> extends State<MultiSelectDialog<V>> {
   bool _showSearch = false;
   List<MultiSelectItem<V>> _items;
   int _selectLimit;
-  // List<V> _limitedSelectedValues = [];
+  // List<V> _limitedSelectedValues = [];f
 
   _MultiSelectDialogState(this._items);
 
